a731b69d11f7e44f4b6e077199c5441a2c061c55
Gautam Sutar <gautamsutar.in@gmail.com>
2025-10-03 14:09:52 +0530

    auth.ts file updated

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,11 +1,22 @@
 import axiosInstance from './axiosInstance';
 
-export const loginUser = async (credentials) => {
+export interface LoginCredentials {
+    email: string;
+    password: string;
+}
+
+export interface SignupData {
+    username: string;
+    email: string;
+    password: string;
+    gender: "male" | "female" | "other";
+}
+export const loginUser = async (credentials:LoginCredentials) => {
     const response = await axiosInstance.post('/users/obtainToken/', credentials);
     return response.data;
 };
 
-export const signupUser = async (userData) => {
+export const signupUser = async (userData:SignupData) => {
     const response = await axiosInstance.post('/users/signup/', userData);
     return response.data;
-};  
+};
